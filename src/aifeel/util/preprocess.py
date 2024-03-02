4ef62e79201446ff117494efc16d5e5a7785c972
panhaGKP <panhajack012@gmail.com>
2024-03-02 23:38:08 +0700

    update preprocessing and export model

--- src/aifeel/util/preprocess.py
+++ src/aifeel/util/preprocess.py
@@ -1,5 +1,6 @@
 import re
 import nltk
+from nltk import ngrams
 from nltk.corpus import stopwords
 
 HTML_TAGS_PATTERN = re.compile(r"<.*?>")
@@ -16,6 +17,26 @@ keep_words = [
     "your",
     "no",
     "not",
+    "nor",
+    "have",
+    "has",
+    "had",
+    "should",
+    "shouldn't",
+    "haven't",
+    "hasn't",
+    "hadn't",
+    "can",
+    "can't",
+    "must",
+    "mustn't",
+    "would",
+    "wouldn't",
+    "will",
+    "won't",
+    "shall",
+    "shan't",
+    "might",
 ]
 
 for word in keep_words:
@@ -59,14 +80,49 @@ def remove_stopwords(text: str) -> str:
     return " ".join(tokens)
 
 
+# def break_contractions(text: str) -> str:
+#     """Break contractions in text."""
+#     mappings = {
+#         "i'm": "i am",
+#         "i'd": "i would",
+#         "i'll": "i will",
+#         "i've": "i have",
+#         "you're": "you are",
+#         "you'd": "you would",
+#         "you'll": "you will",
+#         "you've": "you have",
+#         "he's": "he is",
+#         "he'd": "he would",
+#         "he'll": "he will",
+
+#     }
+
+
+def funny_kind_of_preprocessing(text: str) -> str:
+    """Negation handling."""
+    tokens = nltk.word_tokenize(text)
+    new_tokens = [tokens[0]]
+    for pt, nt in ngrams(tokens, 2):
+        if pt.endswith("n't") or pt in ["no", "not", "nor"]:
+            new_tokens.append("NOT_" + nt)
+        else:
+            new_tokens.append(nt)
+
+    return " ".join(new_tokens)
+
+
 def preprocess_text(text: str) -> str:
     """Preprocess text."""
+    og_text = text
     text = remove_html_tags(text)
     text = remove_links(text)
     text = remove_special_characters(text)
     text = remove_extra_spaces(text)
-    text = to_lowercase(text)
-    text = remove_stopwords(text)
+    text = to_lowercase(text).strip()
+    if not text:
+        return text
+    text = funny_kind_of_preprocessing(text)
+    # text = remove_stopwords(text)
     return text
 
 
