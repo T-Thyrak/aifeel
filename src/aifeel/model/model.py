36b0bb7c1e8af5d376688cd2ecca1533da41a851
T-Thyrak <bus.teng.thaisothyrak@gmail.com>
2024-03-05 22:01:06 +0700

    wip: update NNclassifier to be compatible with scikit-learn

--- src/aifeel/model/model.py
+++ src/aifeel/model/model.py
@@ -16,20 +16,21 @@ class Model(ABC, Generic[T, U]):
     def __init__(self):
         super().__init__()
 
-        self._is_fitted = False
+        # self.is_fitted_ = False
 
     def fit(self: Self, X: T, y: U) -> Self:
         fitted_model = self._fit(X, y)
-        self._is_fitted = True
+        self.is_fitted_ = True
         return fitted_model
 
     def predict(self: Self, X: T) -> U:
-        if not self._is_fitted:
+        # if not self.is_fitted_:
+        if not hasattr(self, "is_fitted_"):
             raise RuntimeError(f"{self.__class__.__name__} is not fitted yet.")
         return self._predict(X)
 
     def save(self: Self, path: str | PathLike) -> None:
-        if not self._is_fitted:
+        if not hasattr(self, "is_fitted_"):
             raise RuntimeError(f"{self.__class__.__name__} is not fitted yet.")
         self._save(path)
 
